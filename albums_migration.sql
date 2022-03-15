6c2c73535994f3e6a841ac8eb464ca8f8ccd2ebe
Cody Hodges <hodges.cody00@gmail.com>
2022-03-15 15:16:43 -0500

    SQL

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,7 +1,6 @@
 USE codeup_test_db;
 
 CREATE TABLE IF NOT EXISTS albums (
-
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     name VARCHAR(50) DEFAULT 'NONE',
     aritist  VARCHAR(50) DEFAULT 'NONE',
@@ -9,5 +8,4 @@ CREATE TABLE IF NOT EXISTS albums (
     sales INT UNSIGNED NOT NULL,
     genre VARCHAR(50) DEFAULT 'NONE',
     PRIMARY KEY (id)
-
 );
