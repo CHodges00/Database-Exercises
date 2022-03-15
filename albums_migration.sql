7bd7c188f996703d908b819bb4cae80fd801ce0b
Cody Hodges <hodges.cody00@gmail.com>
2022-03-15 15:43:55 -0500

    SQL

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+
+CREATE TABLE IF NOT EXISTS albums
+(
+    id           INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name         VARCHAR(50) DEFAULT 'NONE',
+    artist      VARCHAR(50) DEFAULT 'NONE',
+    release_date DATE,
+    sales        INT UNSIGNED NOT NULL,
+    genre        VARCHAR(50) DEFAULT 'NONE',
+    PRIMARY KEY (id)
+);
