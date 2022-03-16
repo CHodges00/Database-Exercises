a2294cd1707d7ce1dc672489771d9d9bf8bbd871
Cody Hodges <hodges.cody00@gmail.com>
2022-03-16 08:43:52 -0500

    SQL

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,8 +1,8 @@
 USE codeup_test_db;
 
+DROP TABLE IF EXISTS quotes;
 
-CREATE TABLE IF NOT EXISTS albums
-(
+CREATE TABLE albums(
     id           INT UNSIGNED NOT NULL AUTO_INCREMENT,
     name         VARCHAR(50) DEFAULT 'NONE',
     artist      VARCHAR(50) DEFAULT 'NONE',
