a772f27fac554c1a7e9bb0037c481ff65dd97f82
Cody Hodges <hodges.cody00@gmail.com>
2022-03-17 14:01:01 -0500

    SQL Exercises

--- albums_migration.sql
+++ albums_migration.sql
@@ -6,7 +6,7 @@ CREATE TABLE albums(
     id           INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist      VARCHAR(50) DEFAULT 'NONE',
     name         VARCHAR(50) DEFAULT 'NONE',
-    release_date YEAR,
+    release_date INT UNSIGNED,
     sales        INT UNSIGNED NOT NULL,
     genre        VARCHAR(50) DEFAULT 'NONE',
     PRIMARY KEY (id)
--- update_exercises.sql
+++ update_exercises.sql
@@ -1,13 +1,21 @@
 USE codeup_test_db;
 
-# All albums in table
+# All albums in table sales * 10
 SELECT * FROM albums;
 UPDATE albums
-SET sales = sales * 10;
+SET sales = albums.sales * 10;
 SELECT * FROM albums;
 
 # All albums before 1980
 SELECT * FROM albums WHERE release_date < 1980;
+UPDATE albums
+SET release_date = 1850
+WHERE 1980 > release_date;
+SELECT * FROM albums WHERE release_date < 1980;
 
 # All albums by Michael Jackson
-SELECT * FROM albums WHERE artist = 'Michael Jackson'
+SELECT * FROM albums WHERE artist = 'Michael Jackson';
+UPDATE albums
+SET artist = 'Peter Jackson'
+WHERE artist = 'Michael Jackson';
+SELECT * FROM albums WHERE artist = 'Peter Jackson';
