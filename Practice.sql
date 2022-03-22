8d56a3130484caf7ca382ef482bfe07fea0e8309
Cody Hodges <hodges.cody00@gmail.com>
2022-03-22 15:11:06 -0500

    Practice

--- Practice.sql
+++ Practice.sql
@@ -74,51 +74,103 @@
 
 
 
-use practice_db;
+# use practice_db;
+#
+# DROP TABLE IF EXISTS authors;
+#
+# DROP TABLE IF EXISTS users;
+# DROP TABLE IF EXISTS roles;
+#
+# CREATE TABLE roles
+# (
+#     id   INT UNSIGNED NOT NULL AUTO_INCREMENT,
+#     name VARCHAR(100) NOT NULL,
+#     PRIMARY KEY (id)
+# );
+#
+# CREATE TABLE users
+# (
+#     id      INT UNSIGNED NOT NULL AUTO_INCREMENT,
+#     name    VARCHAR(100) NOT NULL,
+#     email   VARCHAR(100) NOT NULL,
+#     role_id INT UNSIGNED DEFAULT NULL,
+#     PRIMARY KEY (id),
+#     FOREIGN KEY (role_id) REFERENCES roles (id)
+# );
+#
+# INSERT INTO roles (name)
+# VALUES ('admin');
+# INSERT INTO roles (name)
+# VALUES ('author');
+# INSERT INTO roles (name)
+# VALUES ('reviewer');
+# INSERT INTO roles (name)
+# VALUES ('commenter');
+#
+# INSERT INTO users (name, email, role_id)
+# VALUES ('bob', 'bob@example.com', 1),
+#        ('joe', 'joe@example.com', 2),
+#        ('sally', 'sally@example.com', 3),
+#        ('adam', 'adam@example.com', 3),
+#        ('jane', 'jane@example.com', null),
+#        ('mike', 'mike@example.com', null);
+#
+#
+# SHOW TABLES ;
+# DESCRIBE roles;
+# DESCRIBE users;
+#
+#
+# SELECT * FROM roles;
+# SELECT * FROM users;
+#
+#
+# # For a basic join, we can expect that we will get only the results where both the left and right tables have values for
+# # their respective keys that are mentioned in the ON part of the query.
+# SELECT users.name as user_name, roles.name as role_name
+# FROM users
+#          JOIN roles ON users.role_id = roles.id;
+#
+#
+# # Since we know that there are six users in the database, and users is the left table of the query,
+# # we will get all the users returned, regardless of whether or not they have a role.
+# SELECT users.name AS user_name, roles.name AS role_name
+# FROM users
+#          LEFT JOIN roles ON users.role_id = roles.id;
+#
+#
+# # Since we know that there are four roles in the database, and roles is the right table of the query,
+# # we will get all the roles returned regardless of whether or not there is a user that has that role.
+# SELECT users.name AS user_name, roles.name AS role_name
+# FROM users
+#          RIGHT JOIN roles ON users.role_id = roles.id;
 
-DROP TABLE IF EXISTS authors;
-
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS roles;
-
-CREATE TABLE roles
-(
-    id   INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE users
-(
-    id      INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    name    VARCHAR(100) NOT NULL,
-    email   VARCHAR(100) NOT NULL,
-    role_id INT UNSIGNED DEFAULT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (role_id) REFERENCES roles (id)
-);
-
-INSERT INTO roles (name)
-VALUES ('admin');
-INSERT INTO roles (name)
-VALUES ('author');
-INSERT INTO roles (name)
-VALUES ('reviewer');
-INSERT INTO roles (name)
-VALUES ('commenter');
-
-INSERT INTO users (name, email, role_id)
-VALUES ('bob', 'bob@example.com', 1),
-       ('joe', 'joe@example.com', 2),
-       ('sally', 'sally@example.com', 3),
-       ('adam', 'adam@example.com', 3),
-       ('jane', 'jane@example.com', null),
-       ('mike', 'mike@example.com', null);
 
 
-SHOW TABLES ;
-DESCRIBE roles;
-DESCRIBE users;
 
-SELECT * FROM roles;
-SELECT * FROM users;
+
+
+
+
+
+
+
+
+
+
+
+
+use employees;
+
+# Let us try to find the full name and department for the employee with an employee id of 10001:
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS full_name, d.dept_name
+FROM employees as e
+         JOIN dept_emp as de
+              ON de.emp_no = e.emp_no
+         JOIN departments as d
+              ON d.dept_no = de.dept_no
+WHERE de.to_date = '9999-01-01' AND e.emp_no = 10001;
+
+
+
+
