b2a464920c5666a508a20f77390707e60a0190e2
Cody Hodges <hodges.cody00@gmail.com>
2022-03-24 10:38:44 -0500

    Practice

--- Practice.sql
+++ Practice.sql
@@ -159,18 +159,40 @@
 
 
 
+#
+# use employees;
+#
+# # Let us try to find the full name and department for the employee with an employee id of 10001:
+# SELECT CONCAT(e.first_name, ' ', e.last_name) AS full_name, d.dept_name
+# FROM employees as e
+#          JOIN dept_emp as de
+#               ON de.emp_no = e.emp_no
+#          JOIN departments as d
+#               ON d.dept_no = de.dept_no
+# WHERE de.to_date = '9999-01-01' AND e.emp_no = 10001;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 use employees;
 
-# Let us try to find the full name and department for the employee with an employee id of 10001:
-SELECT CONCAT(e.first_name, ' ', e.last_name) AS full_name, d.dept_name
-FROM employees as e
-         JOIN dept_emp as de
-              ON de.emp_no = e.emp_no
-         JOIN departments as d
-              ON d.dept_no = de.dept_no
-WHERE de.to_date = '9999-01-01' AND e.emp_no = 10001;
-
-
-
-
+SELECT first_name, last_name, birth_date
+FROM employees
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM dept_manager
+)
+LIMIT 10;
