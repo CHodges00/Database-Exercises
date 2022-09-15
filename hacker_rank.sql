b1f745147f8b236694f0ac779943d1039eb1205d
Cody Hodges <hodges.cody00@gmail.com>
2022-09-14 23:32:16 -0500

    HackerRank Practice

--- hacker_rank.sql
+++ hacker_rank.sql
@@ -1,7 +1,9 @@
 # use practice_db;
 
-
-
+#
+#
+#
+# The CITY table is described as follows:
 DROP TABLE IF EXISTS city;
 
 CREATE TABLE city(
@@ -37,3 +39,33 @@ SELECT * from city where countrycode = 'JPN';
 
 # Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN.
 SELECT name from city where countrycode = 'JPN';
+
+#
+#
+#
+# The STATION table is described as follows:
+DROP TABLE IF EXISTS station;
+
+CREATE TABLE station (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    city VARCHAR(21),
+    state VARCHAR(2),
+    lat_n INT,
+    long_w INT,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO station (city, state, lat_n, long_w) VALUES
+    ('Macon', 'GA', 21, 45),
+    ('San Antonio', 'TX', 34, 78);
+
+# Query a list of CITY and STATE from the STATION table.
+SELECT city, state from station;
+
+# Query a list of CITY names from STATION for cities that have an even ID number.
+# Print the results in any order, but exclude duplicates from the answer.
+SELECT DISTINCT city from station where id%2 = 0;
+
+# Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
+SELECT COUNT(CITY) - COUNT(DISTINCT CITY) FROM STATION;
+
