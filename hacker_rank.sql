6ca1b3c7f540bb7cc7b6b81f3f888b633d672a0c
Cody Hodges <hodges.cody00@gmail.com>
2022-09-14 23:58:13 -0500

    HackerRank Practice

--- hacker_rank.sql
+++ hacker_rank.sql
@@ -67,11 +67,29 @@ SELECT city, state from station;
 SELECT DISTINCT city from station where id%2 = 0;
 
 # Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
-SELECT COUNT(CITY) - COUNT(DISTINCT CITY) FROM STATION;
+SELECT COUNT(city) - COUNT(DISTINCT city) FROM STATION;
 
 # Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name).
 # If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
 SELECT city, length(city) from station order by length(city), city limit 1; select city, length(city) from station order by length (city) desc limit 1;
 
 # Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
-SELECT DISTINCT city from station WHERE CITY REGEXP '^[aeiou]';
+SELECT DISTINCT city from station WHERE city REGEXP '^[aeiou]';
+
+# Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+SELECT DISTINCT city from station WHERE city REGEXP '[aeiou]$';
+
+# Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
+SELECT DISTINCT city from station WHERE city REGEXP '[aeiou]$' and city REGEXP '^[aeiou]';
+
+# Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT city FROM STATION WHERE city NOT IN (SELECT city FROM STATION WHERE city REGEXP '^[aeiou]');
+
+# Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT city FROM STATION WHERE city NOT IN (SELECT city from station WHERE city REGEXP '[aeiou]$');
+
+# Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT city FROM STATION WHERE city NOT IN (SELECT city FROM STATION WHERE city REGEXP '^[aeiou]') or city NOT IN  (SELECT city from station WHERE city REGEXP '[aeiou]$');
+
+
+
