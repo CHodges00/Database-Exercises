d80cf336078fc44b64ba74ce8f651835833cf68e
Cody Hodges <hodges.cody00@gmail.com>
2022-09-14 22:58:10 -0500

    HackerRank Practice

--- hacker_rank.sql
+++ hacker_rank.sql
@@ -1,10 +1,12 @@
 use practice_db;
+
+
 # Query all columns for all American cities in the CITY table with populations larger than 100000.
 # The CountryCode for America is USA.
 DROP TABLE IF EXISTS city;
 
 CREATE TABLE city(
-                       id           INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                       id INT UNSIGNED NOT NULL AUTO_INCREMENT,
                        name VARCHAR(17),
                        countrycode VARCHAR(3),
                        district VARCHAR(20),
@@ -12,4 +14,6 @@ CREATE TABLE city(
                        PRIMARY KEY (id)
 );
 
-DESCRIBE city;
+INSERT INTO  city (name, countrycode, district, population) VALUES
+('Atlanta', 312, 'Georgia', 200000),
+('Miami', 786, 'Florida', 20);
